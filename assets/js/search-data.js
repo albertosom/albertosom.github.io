0fed51e40030223889c7d1d2639d4556ee59b3f6
albertosom <46611324+albertosom@users.noreply.github.com>
2025-04-12 18:55:32 +0000

    Deploying to gh-pages from @ albertosom/albertosom.github.io@e47c652a86b8175287a1bb0d2a81039841a0b99f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "See below for my CV. If you want a PDF version, click the PDF file to the right of this text.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
