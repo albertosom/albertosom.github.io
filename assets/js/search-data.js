d24e1cbf415724e65ccd9e66fc68c7b790a90e65
albertosom <46611324+albertosom@users.noreply.github.com>
2026-03-24 22:38:16 +0000

    Deploying to gh-pages from @ albertosom/albertosom.github.io@8f0d7993392daf32b266487c66c18e3187432533 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,13 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
         },{id: "nav-cv",
           title: "cv",
           description: "See below for my CV. If you want a PDF version, click the PDF file to the right of this text.",
