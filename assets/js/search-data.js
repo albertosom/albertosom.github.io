91d25e8c8571aed6bf44fd876c6b3fc7b74147b6
albertosom <46611324+albertosom@users.noreply.github.com>
2026-03-24 22:18:53 +0000

    Deploying to gh-pages from @ albertosom/albertosom.github.io@79709299e4bf789ef3a7b24ab8cc79c3d2deec8e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,13 +23,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-repositories",
-          title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/repositories/";
-          },
         },{id: "nav-cv",
           title: "cv",
           description: "See below for my CV. If you want a PDF version, click the PDF file to the right of this text.",
